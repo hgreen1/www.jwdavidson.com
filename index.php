a0d719040d9509870f29aa5c4949b58f8164ddac
Henry R Green <hgreen1@hawk.iit.edu>
2018-02-05 23:59:06 -0600

    Initial Commit
    Adding a gitignore and making the basic structure of the site.

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>J.W. Davidson Heating, Cooling, & Refridgeration</title>
+	
+</head>
+<body>
+	<!-- So I only have to write these sections once. -->
+	<?php require_once 'topSection.html'; ?>
+	<?php require_once 'menubar.html'; ?>
+
+	<?php require_once 'bottomSection.html'; ?>
+</body>
+</html>
